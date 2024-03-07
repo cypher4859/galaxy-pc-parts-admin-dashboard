99ac355cd10f9f9e8751edd154cd7bca34b77170
Dakota <dakotacustompcs@gmail.com>
2024-03-06 20:59:19 -0500

    Setup TailwindCSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./views/*.{html,js,ejs}', './views/**/*.{html,js,ejs}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
